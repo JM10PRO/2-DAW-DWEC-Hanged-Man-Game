bfbc09edd54039aa9f3528c6668ef54d7f0b3835
José María <josemariagl99@gmail.com>
2022-11-11 12:31:46 +0100

    muestra tabla con bootstrap

--- Ejemplo1/dameprovincias.php
+++ Ejemplo1/dameprovincias.php
@@ -7,10 +7,10 @@ $conexion = mysqli_connect("localhost",$username,$password,$database);
 
 $query = mysqli_query($conexion,"SELECT * FROM tbl_provincias");
 
-echo '<select>';
-//echo '<tr><th>ID</th><td>provincias</td></tr>';
+echo '<table class="table table-striped">';
+echo '<tr><th>ID</th><td>provincias</td></tr>';
 
 while($row = mysqli_fetch_assoc($query)){
-    echo "<option value=".$row['cod'].">".$row['nombre']."</option>";
+    echo "<tr><td>".$row['cod']."</td><td>".$row['nombre']."</td></tr></td>";
 }
-echo '</select>';
+echo '</table>';
